6bd40854d2ac155d0289c205f366c1017ee0abdc
a118skr <98032893+a118skr@users.noreply.github.com>
2022-06-13 14:04:29 +0000

    [Add]SignUpviewにnameフィールド追加、許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  #diveseで作ったUserのストロングパラメータ追加
+  #デフォルトにないnameの受取を許可
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
 end
